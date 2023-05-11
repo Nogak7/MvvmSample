3440d9ebf5e40e490517ffde8f18713156e46538
User <User@LAB3-15>
2023-05-11 14:33:43 +0300

    111

--- MvvmSample/ViewModels/TeacherPageViewModel.cs
+++ MvvmSample/ViewModels/TeacherPageViewModel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MvvmSample.ViewModels
 {
-    public class TeacherPageViewModel:ViewModel
+    public class TeacherPageViewModel : ViewModel
     {
+        private string text;
+
+        public string Text { get { return text; } set { if (text != value) { text = value;  OnPropertyChanged(); } } }
+        private string _text;
+        public ICommand ChangeText { get; set; }
+
+        public TeacherPageViewModel()
+        {
+            ChangeText = new Command(async () => Text = await AppShell.Current.DisplayPromptAsync("enter", "enter name" , "aprove"));
+        }
     }
 }
